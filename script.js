f8e9d44f9e25fb1c1e198bbce51fb8625ed6d649
Veera <veera7032796676@gmail.com>
2025-05-24 14:36:21 +0530

    Initial commit with Maven WhatsApp login project

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 function showAlert() {
   alert("Form submitted!");
 }
+=======
+function showAlert() {
+  alert("Form submitted!");
+}
+>>>>>>> ef9a727 (Initial commit with Maven WhatsApp login project)
